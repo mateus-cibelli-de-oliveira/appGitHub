f36ec0fe92660803a0b6434bc21f4cb232792988
Mateus Cibelli de Oliveira <mateuscibelli@hotmail.com>
2024-10-19 19:21:53 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import React from 'react'
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
+
+const App = () => {
+    
+    <div className='app'>
+
+        <Search />
+        <UserInfo />
+        <Actions />
+
+        <Repos className='repos'
+            title='Repositórios:'
+            repos={[{
+                name: 'Nome do repositório',
+                link: '#'
+            }]}
+        />
+
+        <Repos className='starred'
+            title='Favoritos:'
+            repos={[{
+                name: 'Nome do repositório',
+                link: '#'
+            }]}
+        />
+    </div>
+
+}
+
+export default App
---
+++ src/components/user-info.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => {
+    <div className='user-info'>
+        <img src='https://avatars.githubusercontent.com/u/14853976?v=4' />
+        <h1>
+            <a href='https://github.com/fdaciuk'>Fernando Daciuk</a>
+        </h1>
+        <ul className='repos-info'>
+            <li>Repositórios: 2</li>
+            <li>Seguidores: 1</li>
+            <li>Seguindo: 1</li>
+        </ul>
+    </div>
+}
+
+export default UserInfo
